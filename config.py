6bf2cd70edafcec9eaefcc321c2581fc92b5d600
sunandhini96 <63030539+sunandhini96@users.noreply.github.com>
2023-08-12 03:23:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,20 @@ import albumentations as A
 import cv2
 import torch
 import os
+
 from albumentations.pytorch import ToTensorV2
-#from utils import seed_everything
+from utils import seed_everything
 
 DATASET = '/content/drive/MyDrive/sunandini/pascal/PASCAL_VOC'
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-# seed_everything()  # If you want deterministic behavior
-NUM_WORKERS = min(os.cpu_count(), 4)
+seed_everything()  # If you want deterministic behavior
+NUM_WORKERS = os.cpu_count()-1
 BATCH_SIZE = 32
 IMAGE_SIZE = 416
 NUM_CLASSES = 20
 LEARNING_RATE = 1e-5
 WEIGHT_DECAY = 1e-4
-NUM_EPOCHS = 100
+NUM_EPOCHS = 40
 CONF_THRESHOLD = 0.05
 MAP_IOU_THRESH = 0.5
 NMS_IOU_THRESH = 0.45
@@ -99,86 +100,4 @@ PASCAL_CLASSES = [
     "sofa",
     "train",
     "tvmonitor"
-]
-
-COCO_LABELS = ['person',
- 'bicycle',
- 'car',
- 'motorcycle',
- 'airplane',
- 'bus',
- 'train',
- 'truck',
- 'boat',
- 'traffic light',
- 'fire hydrant',
- 'stop sign',
- 'parking meter',
- 'bench',
- 'bird',
- 'cat',
- 'dog',
- 'horse',
- 'sheep',
- 'cow',
- 'elephant',
- 'bear',
- 'zebra',
- 'giraffe',
- 'backpack',
- 'umbrella',
- 'handbag',
- 'tie',
- 'suitcase',
- 'frisbee',
- 'skis',
- 'snowboard',
- 'sports ball',
- 'kite',
- 'baseball bat',
- 'baseball glove',
- 'skateboard',
- 'surfboard',
- 'tennis racket',
- 'bottle',
- 'wine glass',
- 'cup',
- 'fork',
- 'knife',
- 'spoon',
- 'bowl',
- 'banana',
- 'apple',
- 'sandwich',
- 'orange',
- 'broccoli',
- 'carrot',
- 'hot dog',
- 'pizza',
- 'donut',
- 'cake',
- 'chair',
- 'couch',
- 'potted plant',
- 'bed',
- 'dining table',
- 'toilet',
- 'tv',
- 'laptop',
- 'mouse',
- 'remote',
- 'keyboard',
- 'cell phone',
- 'microwave',
- 'oven',
- 'toaster',
- 'sink',
- 'refrigerator',
- 'book',
- 'clock',
- 'vase',
- 'scissors',
- 'teddy bear',
- 'hair drier',
- 'toothbrush'
 ]
