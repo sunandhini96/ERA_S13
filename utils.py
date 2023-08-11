eef55f0915a65eafdd2d2f94eb1f4a7614b28d48
sunandhini96 <63030539+sunandhini96@users.noreply.github.com>
2023-08-11 22:49:54 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -333,7 +333,62 @@ def get_evaluation_bboxes(
     model.train()
     return all_pred_boxes, all_true_boxes
 
+def get_evaluation_bboxes1(
+    batch,
+    model,
+    iou_threshold,
+    anchors,
+    threshold,
+    box_format="midpoint",
+    device="cuda",
+):
+    # make sure model is in eval before get bboxes
+    
+    train_idx = 0
+    all_pred_boxes = []
+    all_true_boxes = []
+    x, labels = batch
+    x = x.to(device)
 
+    with torch.no_grad():
+        predictions = model(x)
+
+    batch_size = x.shape[0]
+    bboxes = [[] for _ in range(batch_size)]
+    for i in range(3):
+        S = predictions[i].shape[2]
+        anchor = torch.tensor([*anchors[i]]).to(device) * S
+        boxes_scale_i = cells_to_bboxes(
+            predictions[i], anchor, S=S, is_preds=True
+        )
+        for idx, (box) in enumerate(boxes_scale_i):
+            bboxes[idx] += box
+
+    # we just want one bbox for each label, not one for each scale
+    true_bboxes = cells_to_bboxes(
+        labels[2], anchor, S=S, is_preds=False
+    )
+
+    for idx in range(batch_size):
+        nms_boxes = non_max_suppression(
+            bboxes[idx],
+            iou_threshold=iou_threshold,
+            threshold=threshold,
+            box_format=box_format,
+        )
+
+        for nms_box in nms_boxes:
+            all_pred_boxes.append([train_idx] + nms_box)
+
+        for box in true_bboxes[idx]:
+            if box[1] > threshold:
+                all_true_boxes.append([train_idx] + box)
+
+        train_idx += 1
+
+    
+    return all_pred_boxes, all_true_boxes
+    
 def cells_to_bboxes(predictions, anchors, S, is_preds=True):
     """
     Scales the predictions coming from the model to
